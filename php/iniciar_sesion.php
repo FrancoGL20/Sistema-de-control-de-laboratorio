8b5210fd9bb3dbdc003b63c68ae3b4f870c2a2ee
FranciscoGL <francgutierrezlopez@gmail.com>
2022-06-20 17:00:45 -0500

    reestructura de carpetas 1

--- php/iniciar_sesion.php
+++ php/iniciar_sesion.php
@@ -3,13 +3,13 @@
     require_once("./validar_inicio_sesion.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Inicio de Sesión</title>
-        <link rel="stylesheet" href="../styles.css">
+        <link rel="stylesheet" href="../css/styles.css">
         <link rel="shortcut icon" href="https://harinaselizondo.com/wp-content/uploads/2017/03/Favicon.png">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
