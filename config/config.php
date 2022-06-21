15495efa5eb63c93851529e74f379745f6777b1e
FranciscoGL <francgutierrezlopez@gmail.com>
2022-06-21 13:54:46 -0500

    quitar config

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$url="http://localhost/sistema-de-control-de-laboratorio";
-
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', "root");
-define('DB', 'sistema_control_laboratorio');
