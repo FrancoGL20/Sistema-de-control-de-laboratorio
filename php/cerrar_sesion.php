cee64c1bfeb0fd6559b41f4aeb6f88e0773a1ab7
roblesbraun <roblesbraun@gmail.com>
2022-06-23 11:34:26 -0500

    fix-cerrarSesion-validarSesion-createdAltaLotes

--- php/cerrar_sesion.php
+++ php/cerrar_sesion.php
@@ -1,8 +1,8 @@
 <?php
-
+require_once("../config/config.php");
 session_start();
 if(isset($_SESSION['sesion'])){
     unset($_SESSION['sesion']);
 }
 session_destroy();
-header("Location: http://localhost/Sistema-de-control-de-laboratorio/index.php");
+header("Location: $url");
--- php/validar_sesion_iniciada.php
+++ php/validar_sesion_iniciada.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once "../config/config.php";
+require_once("../config/config.php");
 
 if(!isset($_SESSION['sesion'])){
-    header("Location: $url/php/iniciar_sesion.php");
+    header("Location: $url");
 }
