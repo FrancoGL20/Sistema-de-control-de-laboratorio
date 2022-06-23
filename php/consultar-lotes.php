f4a564036e5e1e0f7b15d796bb337719f12372b2
roblesbraun <roblesbraun@gmail.com>
2022-06-23 18:09:16 -0500

    Consultar lotes, listo

---
+++ php/consultar-lotes.php
@@ -0,0 +1,73 @@
+<?php
+    require_once("./validar_sesion_iniciada.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultar Lotes</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="shortcut icon" href="https://harinaselizondo.com/wp-content/uploads/2017/03/Favicon.png">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <?php require_once("./navbar.php"); ?> 
+</head>
+
+<body class="background">
+    <div class="container mx-auto flex flex-col items-center p-10">
+        <div class="w-96 mt-5">
+            <h1 class="text-2xl text-center mb-4 font-bold">Consultar Lotes</h1>
+            <form action="" method="">
+                <div class="mb-4">
+                    <label for="numero de lote" class="block text-gray-700 text-sm font-bold mb-2">
+                        Número de Lote
+                    </label>
+                    <input type="text" name="numeroLote" list="lotes" class="shadow appearance-none border border-slate-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" placeholder="Escriba el número de lote..." required>
+                </div>
+                <div class="flex items-center justify-center mt-7">
+                    <button type="submit" name="buscarLote" class="bg-slate-500 hover:bg-psipeDarkGray text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Consultar
+                    </button>
+                </div>
+                <datalist id="lotes">
+                    <option value="00023571"></option>
+                    <option value="00071583"></option>
+                </datalist>
+            </form>
+        </div>
+        <div class="mt-7">
+            <div class="mt-10 h-96 rounded-xl h-96 overflow-y-auto">
+                <table class="table-auto rounded-xl border border-slate-300 w-[34rem]">
+                    <thead class="rounded-lg">
+                        <tr class="bg-gray-200">
+                            <th colspan="2" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-300 bg-opacity-75 py-3.5 pl-4 pr-3 text-center text-lg font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">No. Lote 00036764</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white rounded-xl">
+                        <tr>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">Tipo de Harina</td>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-400 sm:pl-6 lg:pl-8">Elizondo</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">Capacidad (en toneladas)</td>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-400 sm:pl-6 lg:pl-8">20</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">Fecha de Creación</td>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-400 sm:pl-6 lg:pl-8">07/05/2022</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">Fecha de Caducidad</td>
+                            <td class="border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-400 sm:pl-6 lg:pl-8">17/05/2022</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
